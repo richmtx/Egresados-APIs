ae7ac7ccc345f9b5960b0c3c661003a7637f1a6e
richmtx <martinezhernandezricardo06@gmail.com>
2026-03-12 11:13:16 -0600

    Commit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  //app.setGlobalPrefix('api');
+
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Puerto
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port);
+
+  console.log(`🚀 Servidor corriendo en: http://localhost:${port}`);
+  //console.log(`📡 API disponible en: http://localhost:${port}/api`);
+
 }
+
 bootstrap();
